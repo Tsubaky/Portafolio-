3965d6b9c295f9fd640026dc5045621fe8ed518d
Tsubaky <U22102614@utp.edu.pe>
2024-09-27 17:43:49 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector("nav ul");
+  navBar.classList.toggle("active");
+};
+
+
+
+
+//alert("Hola, este es JavaScript!! <3 ");
+/*
+prompt("Esribe un nombre");
+
+VAriable que se puede modificar
+let nombre ="Alesi";
+var nombre1 = "Agelo";
+Variable con un valor fijo
+const nombre2 = "Pala";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+*/
+
+
+
+
+
+// let contenidoTitulo = "Inicio";
+
+// let tit = document.getElementById("nav1-Inicioid");
+// titulo.innertext = contenidoTitulo;
+
+// /* Condicionales */ 
+// let textoTitulo = titulo.innerText;
+// console.log(textoTitulo);
+
+// if( textoTitulo == "Inicio"){
+//     titulo.innerText = "Otro";
+// }
+
+/* FUNCIONES  */
+  // let pais = "España";
+  // let ciudad = "Madrid";
+
+  // let parrafo = document.querySelector(".informacion-2 .parrafo2-sobre-mi");
+
+  // function cambiarTexto(pais,ciudad) {
+  //   let contenido = `Soy Estudiante de la carrera de Ingeniera de Software.
+  //    Soy de ${pais}, vivo en ${ciudad}.`;
+
+  //    return contenido;
+  // }
+ 
+  // parrafo.textContent = cambiarTexto(pais,ciudad);
+
